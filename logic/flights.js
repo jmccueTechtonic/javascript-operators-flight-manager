c0cdd9355bd1372b7c60350b2aa0ab571ac33051
Joshua McCue <joshua.mccue@techtonic.com>
2020-10-30 19:10:44 -0400

    init commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,32 @@
 function Flights() {
+    function calculateNumberOfFlights (passengers, capacity) {
+        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
 
+        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {
+            throw new Error("The capacity of the flight must be a positive integer value")
+        }
+        let ratio = passengers / capacity;
+        console.log(ratio);
+       if (passengers % capacity === 0) {
+        return ratio;
+       } else {
+        return Math.floor(ratio) + 1;
+       }
+    }
+
+    function checkAircraftRevision(limit, distanceArr) {
+        distanceArr = distanceArr.reduce((acc, el) => acc+=el, 0);
+
+        if (distanceArr <= limit/2) {
+            return "The revision needs to be done within the next 3 months";
+        } else if (distanceArr > limit/2 && distanceArr <= limit*.75) {
+            return "The revision needs to be done within the next 2 months";
+        } else if (distanceArr > limit*.75 && distanceArr <= limit) {
+            return "The revision needs to be done within the next month";
+        } else if (distanceArr > limit) throw new Error("Error");
+    }
+    return {calculateNumberOfFlights, checkAircraftRevision}; 
 }
-
+module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,53 @@
 function Passengers() {
+  function checkFlightCapacity(capacity, passArr) {
+    passArr = passArr.reduce((acc, el) => acc+=el, 0);
 
+    if (passArr < capacity) return passArr;
+    if (passArr > capacity) throw new Error("Error");
+  }
+
+    function distributeAllSeatsToAllPassengers(vip, reg, flights, busSeats, econSeats) {
+      let vipWbusSeats = 0, vipWeconSeats = 0, regWbusSeats = 0, regWeconSeats = 0;
+      
+      let vbMin = Math.min(vip, busSeats);
+      let veMin = Math.min(vip, econSeats);
+
+      let rbMin = Math.min(reg, busSeats);
+      let reMin = Math.min(reg, econSeats);
+
+      while (vbMin > 0) {
+        vipWbusSeats += vbMin;
+        vip -= vbMin;
+        busSeats -= vbMin;
+      }
+
+      while (veMin > 0) {
+        vipWeconSeats += veMin;
+        vip -= veMin;
+        econSeats -= veMin;
+      }
+
+      while (rbMin > 0) {
+        regWbusSeats += rbMin;
+        reg -= rbMin;
+        busSeats -= rbMin;
+      }
+
+      while (reMin > 0) {
+        regWeconSeats += reMin;
+        reg -= reMin;
+        econSeats -= reMin;
+      }
+
+      return {
+        vipWbusSeats,
+        vipWeconSeats,
+        regWbusSeats,
+        regWeconSeats
+      }
+    }
+
+  return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
 
+module.exports = Passengers();
