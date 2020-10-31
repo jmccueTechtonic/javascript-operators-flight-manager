8d6cc7fd3cf46f5e9e6f25445a7b29be78131114
Joshua McCue <joshua.mccue@techtonic.com>
2020-10-30 20:34:08 -0400

    Completed module

--- logic/util.js
+++ logic/util.js
@@ -1,6 +1,13 @@
-function Util() {
-  function calculateTotalDistributedPassengers() {
-    
-  }
-}
 
+function Util() {
+  function calculateTotalDistributedPassengers(distributedPassengers) {
+    return Object.values(distributedPassengers).reduce((acc, el) => acc+el, 0);
+  }
+
+  function calculateTotalNumberOfPassengers(passArr) {
+    return passArr.reduce((acc, el) => acc+=el, 0);
+  }
+
+  return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+}
+module.exports = Util();
